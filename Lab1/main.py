0d34a6b2b0f8328680704ddbaa62a2b6cd2c3a74
Ekaterina <Lazareva-katya@tut.by>
2021-04-19 13:19:00 +0300

    Correct mistakes

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,44 +1,62 @@
-def test1(nums):
-    for i in range(len(nums)):
-        lowest_value_index = i
-        for j in range(i + 1, len(nums)):
-            if nums[j] < nums[lowest_value_index]:
-                lowest_value_index = j
-        nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
-    print('Сортировка выборкой')
-    print(nums)
+def heapify(nums, heap, root):
+    largest = root
+    left = (2 * root) + 1
+    right = (2 * root) + 2
+    if left < heap and nums[left] > nums[largest]:
+        largest = left
+    if right < heap and nums[right] > nums[largest]:
+        largest = right
+    if largest != root:
+        nums[root], nums[largest] = nums[largest], nums[root]
+        heapify(nums, heap, largest)
+
+def heap_sort(nums):
+    n = len(nums)
+    for i in range(n, -1, -1):
+        heapify(nums, n, i)
+    for i in range(n - 1, 0, -1):
+        nums[i], nums[0] = nums[0], nums[i]
+        heapify(nums, i, 0)
+
+def test1():
+    random_nums = [46, 18, 1, 35, 26]
+    heap_sort(random_nums)
+    print('Пирамидальная сортировка')
+    print(random_nums)
+
+def merge(left_list, right_list):
+    sorted = []
+    left_index = right_index = 0
+    left_length, right_length = len(left_list), len(right_list)
+    for _ in range(left_length + right_length):
+        if left_index < left_length and right_index < right_length:
+            if left_list[left_index] <= right_list[right_index]:
+                sorted.append(left_list[left_index])
+                left_index += 1
+            else:
+                sorted.append(right_list[right_index])
+                right_index += 1
+        elif left_index == left_length:
+            sorted.append(right_list[right_index])
+            right_index += 1
+        elif right_index == right_length:
+            sorted.append(left_list[left_index])
+            left_index += 1
+    return sorted
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums) // 2
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+    return merge(left_list, right_list)
 
 def test2():
-    fib1 = fib2 = 1
-    n = 10
-    print(fib1, fib2, end=' ')
-    for i in range(2, n):
-        fib1, fib2 = fib2, fib1 + fib2
-        print(fib2, end=' ')
+    random_nums = [64, 83, 50, 168, 5]
+    random_nums = merge_sort(random_nums)
+    print('Сортировка слиянием')
+    print(random_nums)
 
-def test3(nums):
-    swapped = True
-    while swapped:
-        swapped = False
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
-                nums[i], nums[i + 1] = nums[i + 1], nums[i]
-                swapped = True
-    print('Сортировка пузырьком')
-    print(nums)
-
-def main_activity():
-    num = int(input())
-    if num == 1:
-        random_list_of_nums = [12, 8, 3, 20, 11]
-        test1(random_list_of_nums)
-    elif num == 2:
-        print('Нахождение n-го числа Фибоначчи')
-        test2()
-    elif num == 3:
-        random_list_of_nums = [5, 2, 1, 8, 4]
-        test3(random_list_of_nums)
-    else:
-        print('Ошибка! Неверное число!')
-
-main_activity()
+test1()
+test2()
